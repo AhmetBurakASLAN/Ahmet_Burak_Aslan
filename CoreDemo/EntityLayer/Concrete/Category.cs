25fa16e32a78de18b1a02ef2b07eb54e98afdeb7
AhmetBurakASLAN <aslankaplan0001@gmail.com>
2022-05-16 23:32:16 +0300

    Blog Projesi
    Blog Projesi

---
+++ CoreDemo/EntityLayer/Concrete/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        //erişimbelirleyici türü-değişken türü- isim - {get set}
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+        public bool CategoryStatus { get; set; }
+
+    }
+}
